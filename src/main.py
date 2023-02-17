0b0b6d5d90a8deab71d7d128f59f97d60e5bb046
Pashkov K.V <pk69@kruger.su>
2023-02-17 13:12:15 +0700

    Update main.py
    Внесены изменения в файл main.py

--- src/main.py
+++ src/main.py
@@ -67,21 +67,52 @@ def main():
 
    
    path = Path("config", "config.ini") 
-   
-   logger.info(u'                                                      ')   
    logger.info("Start programs")
+   logger.info("Current path " + str(cPath))
+
    
    f = '*flg'
    #tlgrm.send_telegram("Start programs")
-   
    #catalog = rc._sections.one_C.cat_skl
-   
-   
    #c_shop = file_wr.find_change(catalog, f)
+   
+   
+   
    c_shop = []
    # Apoc по магазинам с изменения
-   
+   tData = db.workDb(rc)
+   rec_con = m_request.req1C(rc)
    mCount = request.req1C(rc)
+   
+   # Список открытых смен от последнего зафиксированного времени
+   l_workshift_open = tData.get_last_workshift_open()
+   logger.info(u'Number of open cash shifts - ' + str(len(l_workshift_open)))
+   # Если нечего отправлять, то и не отправляем
+   if len(l_workshift_open) > 0:
+      status_code = rec_con.post_workshift_open(l_workshift_open)
+      # Меняем дату в файле только в случае успешного результата работы 1С
+      if status_code == 200:
+            tData.save_new_date_open()
+      else:
+            logger.warning('status_code_open - ' + str(status_code ))
+            
+            
+   # Список закрытых смен от последнего зафиксированного времени
+   l_workshift = tData.get_last_workshift()
+   logger.info(u'Number of closed cash shifts - ' + str(len(l_workshift)))
+   # Если нечего отправлять, то и отправляем
+   if len(l_workshift) > 0:
+      #rec_con = m_request.req1C(rc)
+      status_code = rec_con.post_workshift(l_workshift)
+      # Меняем дату в файле только в случае успешного результата работы 1С
+      if status_code == 200:
+            tData.save_new_date()
+      else:
+            logger.warning('status_code - ' + str(status_code ))
+   
+   tData.close_db_connection()      
+   
+   
    # Анализ в каких магазинах изменения
    c_shop = mCount.getQueryShop()
    #print(c_shop)
@@ -96,6 +127,7 @@ def main():
    logger.info(u'End programs')   
    logger.info(u'*****************************************************************')   
 
+   tData.close_db_connection()
 
 def init_pr():
 
@@ -123,7 +155,8 @@ if __name__ == "__main__":
    if not rc == None:
       main()
    else:
-      logger.info(u'Программа завершила работу')                                      
+      logger.error('Configuration file not found')
+      logger.info('The program has finished its work')
       
    #while True:
    #    schedule.run_pending()
