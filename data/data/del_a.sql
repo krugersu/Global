74ca6a12d86e5f73e7980a1c50cf17be4f28ef4c
Pashkov K.V <pk69@kruger.su>
2023-04-14 15:38:23 +0700

    1

--- data/data/del_a.sql
+++
@@ -1,5 +0,0 @@
--- Новый скрипт в myDB.sqlite.
--- Дата: 27 дек. 2022 г.
--- Время: 21:12:23
-DELETE   FROM invent WHERE  isParent <> ''
-
--- data/data/del_null_count_parent.sql
+++
@@ -1,5 +0,0 @@
--- Новый скрипт в myDB.sqlite.
--- Дата: 27 дек. 2022 г.
--- Время: 21:12:23
-DELETE   FROM invent WHERE parent = 1 and remain = 0.0
-
